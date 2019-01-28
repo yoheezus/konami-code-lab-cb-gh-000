9b3dec993166c4fa7619ff387e8c5618b31293f9
Yohann Lovell <yoyunyo@hotmail.co.uk>
2019-01-28 23:00:45 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -11,6 +11,27 @@ const codes = [
   "a"
 ];
 
+let index = 0;
+
+function onKeyDownHandler(e) {
+    const key = e.key;
+
+    if (key === codes[index]) {
+        index++;
+
+        if (index === codes.length) {
+            alert("Hurray!");
+        }
+    } else {
+        index = 0;
+    }
+}
+
 function init() {
   // your code here
+  var body = document.body
+  body.addEventListener("keydown", e => {
+      console.log(e.key)
+      onKeyDownHandler(e)
+  });
 }
